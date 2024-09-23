eebc6bc16b3ae8a9c1ccd3424b1d0f9b914801ab
Ian Tangney <ian.tangney@kasmweb.com>
2024-09-23 15:39:31 +0000

    KASM-6231 Remove V-235832 as its not compatible with V-235831

--- apply_docker_stigs.sh
+++ apply_docker_stigs.sh
@@ -567,40 +567,6 @@ if [ ! -z "$SHOW_ARTIFACT" ] ; then
   echo "Output: $(cat $DOCKER_DAEMON_JSON_PATH | grep -i log-driver)" 
 fi
 
-if [ $(sudo jq -r '."log-opts"."max-size"' /etc/docker/daemon.json) != 'null' ] && [ $(sudo jq -r '."log-opts"."max-file"' /etc/docker/daemon.json) != 'null' ] ; then
-  log_succes "V-235832" "max-size and max-file are set."
-else
-  if  [ $(sudo jq -r '."log-opts"."max-size"' /etc/docker/daemon.json) == 'null' ] ; then
-    if which jq ; then
-        cat <<< $(sudo jq '."log-opts" |= . + {"max-size": "50m"}' /etc/docker/daemon.json) > /etc/docker/daemon.json
-        log_succes "V-235832" "(1 of 2) max-size has been set by this script, be sure to restart the docker service."
-    else
-        log_failure "V-235832" "(1 of 2) max-size is not explicitly set, unable to fix, jq package not installed."
-        echo "	TIP: add '\"max-size\": "50m"' to /etc/docker/daemon.json and restart the docker service"
-    fi
-  else
-    log_succes "V-235832" "(1 of 2) max-size is set."
-  fi
-  if [ $(sudo jq -r '."log-opts"."max-file"' /etc/docker/daemon.json) == 'null' ] ; then
-    if which jq ; then
-        cat <<< $(sudo jq '."log-opts" |= . + {"max-file": 10}' /etc/docker/daemon.json) > /etc/docker/daemon.json
-        log_succes "V-235832" "(2 of 2) max-file has been set by this script, be sure to restart the docker service."
-    else
-        log_failure "V-235832" "(2 of 2) max-file is not explicitly set, unable to fix, jq package not installed."
-        echo "	TIP: add '\"max-file\": 10' to /etc/docker/daemon.json and restart the docker service"
-    fi
-  else 
-  log_succes "V-235832" "(2 of 2) max-file is set."
-  fi
-fi
-if [ ! -z "$SHOW_ARTIFACT" ] ; then
-  echo "Command: grep -Pi '"max-file"\s*:' /etc/docker/daemon.json"
-  echo "Output: $(grep -Pi '"max-file"\s*:' /etc/docker/daemon.json)" 
-  echo "Command: grep -Pi '"max-size"\s*:' /etc/docker/daemon.json"
-  echo "Output: $(grep -Pi '"max-size"\s*:' /etc/docker/daemon.json)" 
-fi
-
-
 if ! (grep --quiet "syslog-address" /etc/docker/daemon.json) ; then
   jqi '. + {"log-opts": {"syslog-address": "udp://127.0.0.1:25224", "tag": "container_name/{{.Name}}", "syslog-facility": "daemon" }}' /etc/docker/daemon.json
   log_succes "V-235833" "Script configured docker daemon remote syslog settings"
